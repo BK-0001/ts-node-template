d8133c46a5ecd8ff6398898e0818f8698d3c309a
Bart Kang <bart.kang.0719@gmail.com>
2024-06-25 13:45:41 -0700

    add custom error and handle global error

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { CustomError } from "./errors/custom-error";
 
 export const app = express();
 
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
+  throw new CustomError("test", 500);
   res.send("Hello world");
 });
 
@@ -12,4 +14,10 @@ app.all("*", (req: Request, res: Response) => {
   res
     .status(404)
     .json({ error: `Not Found Route - ${req.method} ${req.path}` });
+});
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof CustomError) {
+    res.status(error.statusCode).json(error);
+  }
 });
